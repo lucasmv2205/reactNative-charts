c4b1778fe51c3f77bedca3dde47d7ca7075116af
lucasmv2205 <lmartins2205@gmail.com>
2022-04-16 03:23:18 -0300

    render a native chart to see all the items of your monthly bill

---
+++ README.md
@@ -0,0 +1,21 @@
+# React Native Charts 👨‍💻
+
+This is a react native app to control your bills and see a pie chart(native render) for each item of your monthly bill. You can select an account item and highlight it in the chart.
+
+
+### install dependencies 🧾
+
+```bash
+yarn
+```
+
+### run project 🧾
+
+```bash
+yarn start
+```
+## Tech stack
+- React Native
+- Victory charts
+- react-native-svg
+- Expo
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,29 +1,70 @@
-import React from "react";
-import { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import { VictoryPie, VictoryTooltip } from "victory-native";
 
-import { EXPENSES } from '../utils/expenses';
+import { EXPENSES } from "../utils/expenses";
 
-import { Card, CardProps } from '../components/Card';
-import { Header, MonthsProps } from '../components/Header';
+import { Card, CardProps } from "../components/Card";
+import { Header, MonthsProps } from "../components/Header";
 
-import { Container } from './styles';
+import { Container, Chart } from "./styles";
 
 export function Home() {
+  const [selected, setSelected] = useState("");
   const [month, setMonth] = useState<MonthsProps>("Janeiro");
+  const [data, setData] = useState<CardProps[]>([]);
+
+  function handleCardOnPress(id: string) {
+    setSelected((prev) => (prev === id ? "" : id));
+  }
+
+  useEffect(() => {
+    setData(EXPENSES[month]);
+  }, [month]);
 
   return (
     <Container>
-      <Header
-        onValueChange={setMonth}
-        selectedValue={month}
-      />
+      <Header onValueChange={setMonth} selectedValue={month} />
+      <Chart>
+        <VictoryPie
+          data={data}
+          x="label"
+          y="value"
+          colorScale={data.map((expense) => expense.color)}
+          innerRadius={80}
+          padAngle={3}
+          animate={{
+            duration: 2000,
+            easing: "exp"
+          }}
+          style={{
+            labels: {
+              fill: "#fff",
+            },
+            data: {
+              fillOpacity: ({ datum }) =>
+                datum.id === selected || selected === "" ? 1 : 0.5,
+              stroke: ({ datum }) =>
+                datum.id === selected ? datum.color : "none",
+              strokeWidth: 10,
+              strokeOpacity: 0.5
+            },
+          }}
+          labelComponent={
+            <VictoryTooltip renderInPortal={false} flyoutStyle={{
+              stroke: 0,
+              fill: ({ datum }) => datum.color,
+            }} />
+          }
+        />
+      </Chart>
 
       <FlatList
         data={EXPENSES[month]}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <Card
+            onPress={() => handleCardOnPress(item.id)}
             data={item}
             selected={false}
           />
